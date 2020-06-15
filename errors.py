de7c9b3820400f483aaa7547564d2852347a1437
Horlarwumhe <amachiever4real@gmail.com>
2020-06-15 15:58:19 +1300

    minor changes to errors.py

--- errors.py
+++ errors.py
@@ -7,62 +7,51 @@ Exceptions and exception helpers.
 ### exception during parsing
 
 class AbbeyBaseError(Exception):
-    ''' base errorfor all abbey errors
+    ''' base error for all abbey errors
     '''
+
+# errors in lexing and parsing phases
 class AbbeySyntaxError(AbbeyBaseError):
 
     def __init__(self, message, line, column):
-        super(AbbeySyntaxError, self).__init__(message)
+        super().__init__(message)
         self.message = message
         self.line = line
         self.column = column
 
 class LexerError(AbbeySyntaxError):
-    def __init__(self,msg,line=None,column=None):
-        self.message = msg
-        self.line = line
-        self.column = column
+    pass
 
 class ParserError(AbbeySyntaxError):
 
     def __init__(self, message, token):
-        super(ParserError, self).__init__(message, token.line, token.column)
+        super().__init__(message, token.line, token.column)
+
 
 ## exceptions during excecution
 class AbbeyError(AbbeyBaseError):
 
     def __init__(self, message, line=None, column=None):
-        super(AbbeyError, self).__init__(message)
         self.message = message
         self.line = line
         self.column = column
         self.type = self.__class__.__name__
 
 class NameError_ (AbbeyError):
-    def __init__(self, message,line=None):
-        self.line = line
-        super(NameError_, self).__init__(message,line,None)
+    pass
 
 class AttributeError_(AbbeyError):
-    """docstring for AttributeError"""
-    def __init__(self, message,line=None):
-        self.line = line
-        super(AttributeError_, self).__init__(message,line,None)
+    pass
         
 class TypeError_(AbbeyError):
-    """docstring for AttributeError"""
-    def __init__(self, message,line):
-        self.line = line
-        super().__init__(message,line,None)
+    pass
 
 
 class IndexError_(AbbeyError):
-     def __init__(self, message,line):
-        self.line = line
-        super().__init__(message,line,None)
+    pass
 
 
-class AbbeyModuleError(AbbeySyntaxError):
+class AbbeyModuleError(AbbeyBaseError):
     """ exception when importing a module
     """
     def __init__(self,message,line,source_lines,column=1,file=''):
